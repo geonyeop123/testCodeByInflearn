532a7afa91f1d341eedbc4b59a88a5a5d348ea00
geonyeop123 <64470512+geonyeop123@users.noreply.github.com>
2024-07-18 15:55:02 +0900

    createOrderByduplicateProduct
    중복된 상품으로 오더 만들기
    
    조회된 상품 목록을 map으로 반환하여 중복된 상품을 담을 수 있게 한다.

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -12,6 +12,8 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -26,7 +28,13 @@ public class OrderService {
         List<String> productNumbers = request.getProductNumbers();
         List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
 
-        Order order = Order.create(products, registeredDateTime);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        List<Product> duplicateProducts = productNumbers.stream()
+                .map(productMap::get).collect(Collectors.toList());
+
+        Order order = Order.create(duplicateProducts, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
 
         return OrderResponse.of(savedOrder);
